523c35eaffa0bb5d694b32ad853271ac8cbc8af1
rsaniewski-jpg <rsaniewski@strictus.pl>
2026-09-22 15:04:54 +0200

    Update koszty.py

--- koszty.py
+++ koszty.py
@@ -362,7 +362,7 @@ if not st.session_state.zalogowany:
 
     st.info(
         "👈 Wpisz swój adres e-mail oraz hasło w panelu po lewej stronie i wciśnij **Enter** (lub kliknij 'Zaloguj się')."
-        "\n\n*(Domyślny login administratora to: `admin@firma.pl` / hasło: `0000`)*"
+        #-- "\n\n*(Domyślny login administratora to: `admin@firma.pl` / hasło: `0000`)*" --
     )
     st.stop()
 
